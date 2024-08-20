77248aedb0b7b63730a24798a2311bd221d5f219
Eugene-Khonich <ehonich@gmail.com>
2024-08-20 18:58:06 +0300

    done

--- src/main.js
+++ src/main.js
@@ -1,13 +1,50 @@
 import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
 import { fetchFotos } from './js/pixabay-api';
+import { greateCards } from './js/render-functions';
 
-const searchInput = document.querySelector('.input');
+const form = document.querySelector('.form');
 const galleryList = document.querySelector('.gallery-list');
+
+const lightBox = new SimpleLightbox('.gallery-list a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+});
 
 const onSearchSubmit = e => {
   e.preventDefault();
-  const searchValue = searchInput.elements.user_query.value;
-  console.dir(searchValue);
+  const value = e.target.elements.inputField.value.toLowerCase().trim();
+  if (!value) {
+    iziToast.error({
+      title: 'Error',
+      message: 'You need to type something on field!',
+      position: 'topRight',
+    });
+    return;
+  }
+  galleryList.innerHTML = '<span class="loader"></span>';
+
+  fetchFotos(value)
+    .then(img => {
+      if (img.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        galleryList.innerHTML = '';
+        return;
+      } else {
+        galleryList.innerHTML = '';
+        greateCards(img.hits);
+        lightBox.refresh();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  form.reset();
 };
 
-searchInput.addEventListener('submit', onSearchSubmit);
+form.addEventListener('submit', onSearchSubmit);
